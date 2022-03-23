50d4a8cfc8ff2424e6e3dc3664cb129bcac89719
domcos223 <horvath.dominika223@gmail.com>
2022-03-23 11:13:00 +0100

    styling, adding buttons

--- src/column.jsx
+++ src/column.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import Task from './task';
 import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
+import Button from 'react-bootstrap/Button';
+import { AlignBottom, AlignCenter, AlignMiddle, Plus } from 'react-bootstrap-icons';
+
 
 
 const Container = styled.div`
     margin: 8px;
     border: 1px solid lightgrey;
-    border-radius: 2px;
+    border-radius: 5%;
     width: 220px;
-
+    background: #EFEFEF;
     display: flex;
     flex-direction: column;
+
 `;
 const Title = styled.h3`
     padding: 8px;
     margin: 3px;
 `;
+
 const TaskList = styled.div`
     padding: 8px;
     transition: background-color 0.2 ease;
+    border-radius: 5%;
     background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
     flex-grow: 1;
     min-height: 100px;
@@ -43,7 +49,11 @@ export default class Column extends React.Component {
     render() {
         return (
         <Container>
-            <Title>{this.props.column.title}</Title>
+            <Title>{this.props.column.title}
+            <Button variant="primary" style={{float: "right"}}>
+            <Plus style={{backgroundColor: "#EFEFEF",}}/>
+            </Button>
+            </Title>
             <Droppable
              droppableId={this.props.column.id}
              isDropDisabled={this.props.isDropDisabled}
