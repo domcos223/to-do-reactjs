69da9c7a8990abcce3d28220088830fc1af7d20c
Horváth Dominika Csilla <horvath.dominika223@gmail.com>
2022-04-21 15:50:17 +0200

    data passing, add page

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,37 +1,42 @@
-const initialData = {
-    tasks: {
-        'task-1': {id: 'task-1', content: 'CRUD műveletek'},
-        'task-2': {id: 'task-2', content: 'JSON mock backend'},
-        'task-3': {id: 'task-3', content: 'Feladatokhoz részletek (leírás,határidő)'},
-        'task-4': {id: 'task-4', content: 'UI elemek a műveletekhez'},
-    },
-    columns: {
-        'column-1': {
-        id: 'column-1',
-        title: 'TODO',
-        taskIds: ['task-1', 'task-2', 'task-3','task-4'],
-        },
-        'column-2': {
-            id: 'column-2',
-            title: 'In Progress',
-            taskIds: [],
-            },
-        'column-3': {
-        id: 'column-3',
-        title: 'Done',
-        taskIds: [],
-        },
-        'column-4': {
-            id: 'column-4',
-            title: 'Postponed',
-            taskIds: [],
-            },
-    },
-    columnOrder: ['column-1','column-2','column-3','column-4'],
+//initialData = fetch("localhost:4000/get_all_data")
 
-    buttons: {
-        'addbtn-1': {id: 'addbtn-1'},
+const initialData = {
+  tasks: {
+    "task-1": { id: "task-1", content: "CRUD műveletek" },
+    "task-2": { id: "task-2", content: "JSON mock backend" },
+    "task-3": {
+      id: "task-3",
+      content: "Feladatokhoz részletek (leírás,határidő)",
     },
+    "task-4": { id: "task-4", content: "UI elemek a műveletekhez" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "TODO",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+    },
+    "column-2": {
+      id: "column-2",
+      title: "In Progress",
+      taskIds: [],
+    },
+    "column-3": {
+      id: "column-3",
+      title: "Done",
+      taskIds: [],
+    },
+    "column-4": {
+      id: "column-4",
+      title: "Postponed",
+      taskIds: [],
+    },
+  },
+  columnOrder: ["column-1", "column-2", "column-3", "column-4"],
+
+  buttons: {
+    "addbtn-1": { id: "addbtn-1" },
+  },
 };
 
 export default initialData;
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -2,13 +2,38 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import React from "react";
+import initialData from '../initial-data';
 import "../styles/add.css";
 
 export default class Add extends React.Component {
+  state = initialData;
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id")
+    const title = event.target[0].value
+    const description = event.target[1].value
+    const createdDate = event.target[2].value
+
+    console.log("A backendnek elküldtük az alábbi objektumot.")
+    console.log({
+      id: id,
+      title: title,
+      description: description,
+      createdDate: createdDate
+    })
+    //fetch("POST", {
+//    id: girpgripe
+//  task title: event.taskTitle
+//  description: event.disciption
+//  })
+  }
+
   render() {
     return (
       <Container id="main-container" className="d-grid">
-        <Form id="addform" className="text-center">
+        <Form id="addform" className="text-center" onSubmit={this.handleSubmit} method="GET">
           <h1 className="fs-3 fw-normal">Add new task</h1>
           <Form.Group id="tasktitle">
               <Form.Control type="title" size="lg" placeholder="Task title"/>
@@ -19,8 +44,8 @@ export default class Add extends React.Component {
           <Form.Group id="taskdate">
               <Form.Control type="date" size="lg"/>
           </Form.Group>
-          <a><Button id="submitBtn" type="button" class="btn-light btn-lg">Submit</Button></a>
-          <a><Button id="backBtn" type="button" class="btn btn-warning">Cancel</Button></a>
+          <a><Button id="submitBtn" type="submit" className="btn-light btn-lg">Submit</Button></a>
+          <a><Button id="backBtn" type="button" className="btn btn-warning">Cancel</Button></a>
         </Form>
       </Container>
     );
